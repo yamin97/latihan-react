872bdb8295f0eef9cd55b33e011e66cb28db68b6
Firliandy <lian.eddy@gmail.com>
2019-11-26 11:45:07 +0700

    commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,41 +1,77 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Button } from 'reactstrap'
 import Axios from 'axios'
+import { Table, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
 class Home extends Component{
 
-    componentDidMount(){
-        Axios.get('http://localhost:2000/employees')
-        .then((res) => {
-            console.log(res.data)
-        })
-        .catch((err) => {
-            console.lof(err)
-        })
+    state = {
+        data : [],
+        buah: []
     }
-    contoh = () => {
-        Axios.post('http://localhost:2000/users', {username: 'haha', password: '123'})
+
+    componentDidMount(){
+        Axios.get('http://localhost:2000/users')
         .then((res) => {
-            Axios.get('http://localhost:2000/users')
+            this.setState({data : res.data})
+            console.log(this.state.data)
+            Axios.get('http://localhost:2000/products')
             .then((res) => {
                 console.log(res.data)
+                this.setState({buah : res.data})
             })
         })
         .catch((err) => {
             console.log(err)
         })
     }
+
+    renderUserData = () => {
+        return this.state.data.map((val, index) => {
+            return(
+                    <tr>
+                      <th scope="row">{index+1}</th>
+                      <td>{val.first_name}</td>
+                      <td>{val.last_name}</td>
+                      <td>{val.email}</td>
+                    </tr>
+            )
+        })
+    }
+
     render(){
         return(
             <div>
-                Iniasdsadasdsad Home
-                <Link to='/not-home' className='btn btn-outline-danger' >
-                    not home
-                </Link>
-                <button onClick={this.contoh}>
-                    click me
-                </button>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Username</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.renderUserData()}
+                    </tbody>
+                </Table>
+                <Form>
+                  <FormGroup>
+                    <Label for="first-name">First Name</Label>
+                    <Input type="text" name="first-name" ref="first-name" placeholder="First Name" />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="last-name">Last Name</Label>
+                    <Input type="text" name="last-name" ref="last-name" placeholder="Last Name" />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="email">Email</Label>
+                    <Input type="text" name="email" ref="email" placeholder="Email" />
+                  </FormGroup>
+                  <Button color='primary' onClick={this.submitData}>
+                      Submit
+                  </Button>
+                </Form>
             </div>
         )
     }
